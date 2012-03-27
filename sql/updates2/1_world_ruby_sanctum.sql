d79b3c1109e5067a5aa90405ed2366db11dbbef1
NorthStrider <Dreadkiller@gmx.de>
2012-03-27 14:38:43 +0200

    DB:/RubySanctum: fixed damage of trash and bosses

--- sql/updates2/1_world_ruby_sanctum.sql
+++ sql/updates2/1_world_ruby_sanctum.sql
@@ -1,3 +1,46 @@
+-- damage fix
+
+-- Charscale Invoker
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '7.5' WHERE entry =40417;
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '12.5' WHERE entry =40418;
+ -- Charscale Assaulter
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '7.5' WHERE entry =40419;
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '12.5' WHERE entry =40420;
+ -- Charscale Elite
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '7.5' WHERE entry =40421;
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '12.5' WHERE entry =40422;
+ -- Charscale Commander
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '7.5' WHERE entry =40423;
+ UPDATE creature_template SET mindmg = '422',maxdmg = '586',attackpower = '642',dmg_multiplier = '12.5' WHERE entry =40424;
+
+-- Baltharus the Warborn
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '35' WHERE entry =39751;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '45' WHERE entry =39920;
+
+-- Baltharus Clone
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '35' WHERE entry =39899;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '45' WHERE entry =39922;
+
+-- Saviana Ragefire
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '35' WHERE entry =39747;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '45' WHERE entry =39823;
+
+-- General Zarithrian
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '35' WHERE entry =39746;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '45' WHERE entry =39805;
+
+-- Halion
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '35' WHERE entry =39863;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '45' WHERE entry =39864;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '55' WHERE entry =39944;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '75' WHERE entry =39945;
+
+-- Twilight Halion
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '35' WHERE entry =40142;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '45' WHERE entry =40143;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '55' WHERE entry =40144;
+ UPDATE creature_template SET mindmg = '496',maxdmg = '674',attackpower = '783',dmg_multiplier = '75' WHERE entry =40145;
+
 -- Trash SAI for ruby sanctum
 
 UPDATE `creature_template` SET `AIName`='SmartAI', `ScriptName`='' WHERE `entry` IN (40419, 40421, 40417, 40423);
